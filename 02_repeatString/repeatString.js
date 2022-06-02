dea4b7aafebf6c4bf451bb599afdfeed077adaf2
Artur Rybak <arturybak@outlook.com>
2022-06-02 17:13:26 +0100

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let res = '';
+    while (num) {
+        res += phrase;
+        num--;
+    }
+    return res;
 };
 
 // Do not edit below this line
