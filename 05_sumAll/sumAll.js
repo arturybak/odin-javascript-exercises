68d7fd0390dfc9c8a66446dcd7b285ab51ced5f2
Artur Rybak <arturybak@outlook.com>
2022-06-04 18:14:26 +0100

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    }
 
+    let res = 0;
+
+    if (start > end) [start, end] = [end, start];
+
+    for (start; start <= end; start++) {
+        res += start;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
