cd0ac1580a14fa4f370af3814459d659e9c3c1c5
Artur Rybak <arturybak@outlook.com>
2022-06-04 18:27:31 +0100

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (farenheit) {
+  let conversion = (farenheit - 32) * 5 / 9;
+  return Math.round(conversion * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let conversion = celsius * 9 / 5 + 32;
+  return Math.round(conversion * 10) / 10;
 };
 
 // Do not edit below this line
