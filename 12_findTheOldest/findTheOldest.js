8b557149791b163671fa3213169f8c741cc31dc2
Artur Rybak <arturybak@outlook.com>
2022-06-11 11:02:35 +0100

    12 Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+       const currentYear = (new Date).getFullYear()
+       const oDeath = oldest.yearOfDeath || currentYear;
+       const cDeath = current.yearOfDeath || currentYear;
+       const oAge = oDeath - oldest.yearOfBirth;
+       const cAge = cDeath - current.yearOfBirth;
+       return oAge > cAge ? oldest : current;
+    })
 };
 
 // Do not edit below this line
