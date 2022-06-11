3fb71725ac6d1cfb57d4f7c433caf0163ef5664c
Artur Rybak <arturybak@outlook.com>
2022-06-11 10:41:26 +0100

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const lowered = str.toLowerCase().replace(/[^a-z]/gi,'');
+    const reversed = lowered.split('').reverse().join('');
+    return lowered === reversed;
 };
 
 // Do not edit below this line
