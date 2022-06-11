b22b5de07ab50001b9b395c9900f5a69c35f4636
Artur Rybak <arturybak@outlook.com>
2022-06-11 11:07:44 +0100

    10 Improvements

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,13 @@
-const findTheOldest = function(people) {
-    return people.reduce((oldest, current) => {
-       const currentYear = (new Date).getFullYear()
-       const oDeath = oldest.yearOfDeath || currentYear;
-       const cDeath = current.yearOfDeath || currentYear;
-       const oAge = oDeath - oldest.yearOfBirth;
-       const cAge = cDeath - current.yearOfBirth;
+const findTheOldest = people => people.reduce((oldest, current) => {
+       const oAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+       const cAge = getAge(current.yearOfBirth, current.yearOfDeath)
        return oAge > cAge ? oldest : current;
-    })
-};
+    });
+
+const getAge = (birth, death) => {
+    death = death || new Date().getFullYear();
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
